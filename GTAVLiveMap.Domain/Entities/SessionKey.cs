6509b1da299f88adfee31087088be65f01ffb1a4
Vlad Kovalchuk <vladkovalchukvlad1@gmail.com>
2021-10-15 02:27:58 +0300

    add session key model and migration

---
+++ GTAVLiveMap.Domain/Entities/SessionKey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GTAVLiveMap.Domain.Entities
+{
+    public class SessionKey
+    {
+        public string Key { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastAt { get; set; } = DateTime.Now;
+        public string UserAgent { get; set; }
+        public string CreatedIP { get; set; }
+        public string LastIP { get; set; }
+        public int OwnerId { get; set; }
+    }
+}
