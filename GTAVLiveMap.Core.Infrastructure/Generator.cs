1fb79dff843487bf13833b8630317ed536e4ed6b
Vlad Kovalchuk <vladkovalchukvlad1@gmail.com>
2021-10-15 02:27:01 +0300

    Create Generator.cs

---
+++ GTAVLiveMap.Core.Infrastructure/Generator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GTAVLiveMap.Core.Infrastructure
+{
+    public static class Generator
+    {
+        public static string GetRandomString(int size, bool upperCase = false)
+        {
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[size];
+            var random = new Random();
+
+            for (int i = 0; i < stringChars.Length; i++)
+                stringChars[i] = chars[random.Next(chars.Length)];
+
+            var finalString = new String(stringChars);
+
+            return upperCase ? finalString.ToUpper() : finalString;
+        }
+    }
+}
