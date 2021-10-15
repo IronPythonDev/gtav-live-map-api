9ac1d58b76d8c678122865d394482d9cb7dce2bb
Vlad Kovalchuk <vladkovalchukvlad1@gmail.com>
2021-10-15 11:32:47 +0300

    Created service for get google account by JWT

---
+++ GTAVLiveMap.Core.Infrastructure/Services/GoogleService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading.Tasks;
+using static Google.Apis.Auth.GoogleJsonWebSignature;
+
+namespace GTAVLiveMap.Core.Infrastructure.Services
+{
+    public class GoogleService : IGoogleService
+    {
+        public GoogleService(
+            IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        IConfiguration Configuration { get; }
+
+        public async Task<Payload> GetUserFromJWT(string jwt)
+        {
+            try
+            {
+                Payload payload = await ValidateAsync(jwt, new ValidationSettings
+                {
+                    Audience = new[] { Configuration["AuthProviders:Google:ClientId"] },
+                    ExpirationTimeClockTolerance = TimeSpan.FromDays(1),
+                    IssuedAtClockTolerance = TimeSpan.FromDays(1)
+                });
+
+                return payload;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ GTAVLiveMap.Core.Infrastructure/Services/IGoogleService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using static Google.Apis.Auth.GoogleJsonWebSignature;
+
+namespace GTAVLiveMap.Core.Infrastructure.Services
+{
+    public interface IGoogleService
+    {
+        Task<Payload> GetUserFromJWT(string idToken);
+    }
+}
