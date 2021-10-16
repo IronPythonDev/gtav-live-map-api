4c3daa43e60528e97d1e4c17954465a52a8a8195
Vlad Kovalchuk <vladkovalchukvlad1@gmail.com>
2021-10-16 18:16:53 +0300

    Create DeleteRolesColumnFromMapMembersTable.cs

---
+++ GTAVLiveMap.Core/Migrations/DeleteRolesColumnFromMapMembersTable.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace GTAVLiveMap.Core.Migrations
+{
+    [Migration(20211610175900)]
+    public class DeleteRolesColumnFromMapMembersTable : Migration
+    {
+        public override void Down()
+        {
+            Create.Column("Roles")
+                .OnTable("MapMembers")
+                .AsString()
+                .WithDefaultValue("");
+        }
+
+        public override void Up()
+        {
+            Delete.Column("Roles").FromTable("MapMembers");
+        }
+    }
+}
