4762b64cb38f0788dde2a753d59c7d44fa568764
Vlad Kovalchuk <vladkovalchukvlad1@gmail.com>
2021-10-23 21:46:29 +0300

    Create UpdateInviteDTO.cs

---
+++ GTAVLiveMap.Core.Infrastructure/DTOs/UpdateInviteDTO.cs
@@ -0,0 +1,12 @@
+﻿using GTAVLiveMap.Domain.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace GTAVLiveMap.Core.Infrastructure.DTOs.Requests
+{
+    public class UpdateInviteDTO
+    {
+        public Guid Id { get; set; }
+        public IList<string> Scopes { get; set; }
+    }
+}
