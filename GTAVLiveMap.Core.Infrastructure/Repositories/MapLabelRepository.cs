9a163fd945bc80c5525cc15098d611328db0ae2f
Vlad Kovalchuk <vladkovalchukvlad1@gmail.com>
2021-10-29 16:25:47 +0300

    Update MapLabelRepository.cs

--- GTAVLiveMap.Core.Infrastructure/Repositories/MapLabelRepository.cs
+++ GTAVLiveMap.Core.Infrastructure/Repositories/MapLabelRepository.cs
@@ -30,24 +30,48 @@ namespace GTAVLiveMap.Core.Infrastructure.Repositories
 
         public async Task<IList<MapLabel>> GetAll(int limit = int.MaxValue, int offset = int.MaxValue)
         {
-            return (await DbContext.QueryAsync<MapLabel>(
+            var labels = (await DbContext.QueryAsync<MapLabel>(
                 @"SELECT * FROM public.""MapLabels"" ORDER BY ""Id"" LIMIT @Limit OFFSET @Offset",
                 new { Limit = limit, Offset = offset })).ToList();
+
+            return labels.Select(async l =>
+            {
+                l.CustomId = await GetCustomId(l);
+                return l;
+            }).Select(t => t.Result).ToList();
         }
 
-        public async Task<MapLabel> GetById(Guid id) =>
-            (await DbContext.QueryAsync<MapLabel>(@"SELECT * FROM public.""MapLabels"" WHERE ""Id"" = @Id;", new { Id = id })).FirstOrDefault();
+        public async Task<MapLabel> GetById(Guid id)
+        {
+            var label = (await DbContext.QueryAsync<MapLabel>(@"SELECT * FROM public.""MapLabels"" WHERE ""Id"" = @Id;", new { Id = id })).FirstOrDefault();
+
+            label.CustomId = await GetCustomId(label);
+
+            return label;
+        }
 
         public async Task<IList<MapLabel>> GetByMapId(Guid mapId, int limit = int.MaxValue, int offset = 0)
         {
-            return (await DbContext.QueryAsync<MapLabel>(
+            var labels = (await DbContext.QueryAsync<MapLabel>(
                 @"SELECT * FROM public.""MapLabels"" WHERE ""MapId"" = @MapId ORDER BY ""Id"" LIMIT @Limit OFFSET @Offset",
-                new { MapId = mapId , Limit = limit, Offset = offset })).ToList();
+                new { MapId = mapId, Limit = limit, Offset = offset })).ToList();
+
+            return labels.Select(async l =>
+            {
+                l.CustomId = await GetCustomId(l);
+                return l;
+            }).Select(t => t.Result).ToList();
         }
 
-        public async Task<MapLabel> GetByMapIdAndCustomId(Guid mapId, string customId) =>
-                    (await DbContext.QueryAsync<MapLabel>(@"SELECT * FROM public.""MapLabels"" WHERE ""MapId"" = @MapId AND ""CustomId"" = @CustomId;", 
-                        new { MapId = mapId , CustomId = await GenerateCustomId(new MapLabel { MapId = mapId , CustomId = customId }) })).FirstOrDefault();
+        public async Task<MapLabel> GetByMapIdAndCustomId(Guid mapId, string customId)
+        {
+            var label = (await DbContext.QueryAsync<MapLabel>(@"SELECT * FROM public.""MapLabels"" WHERE ""MapId"" = @MapId AND ""CustomId"" = @CustomId;",
+                        new { MapId = mapId, CustomId = await GenerateCustomId(new MapLabel { MapId = mapId, CustomId = customId }) })).FirstOrDefault();
+
+            label.CustomId = await GetCustomId(label);
+
+            return label;
+        }
 
         public async Task<int> GetCountByMapId(Guid mapId) =>
             (await GetByMapId(mapId)).Count;
@@ -71,6 +95,6 @@ namespace GTAVLiveMap.Core.Infrastructure.Repositories
             Task.FromResult($"{mapLabel.MapId}:{mapLabel.CustomId}");
 
         public Task<string> GetCustomId(MapLabel mapLabel) =>
-            Task.FromResult(mapLabel.CustomId.Split(';').Last());
+            Task.FromResult(mapLabel.CustomId.Split(':').Last());
     }
 }
