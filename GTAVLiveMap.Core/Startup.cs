adc195cdf0f4b3b60f834d165a8bac18e4aea044
Vlad Kovalchuk <vladkovalchukvlad1@gmail.com>
2021-11-02 14:56:29 +0200

    Update Startup.cs

--- GTAVLiveMap.Core/Startup.cs
+++ GTAVLiveMap.Core/Startup.cs
@@ -103,8 +103,6 @@ namespace GTAVLiveMap.Core
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
             app.UseCors("CorsPolicy");
